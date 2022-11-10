8c79f7eee2d511e470f4e48fdb7ac77e2f22c1c9
SandevD <sandev.net@gmail.com>
2022-11-10 15:49:43 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ContactHeader from './components/ContactHeader';
-import ContactForm from './components/ContactForm';
-import ContactGrid from './components/ContactGrid';
 
 const App = () => {
 	return (
