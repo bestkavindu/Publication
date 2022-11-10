29091337f6654508cb62732cdd4b7c7db2867992
SandevD <sandev.net@gmail.com>
2022-11-10 15:32:02 +0530

    🔥 Contact Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ContactHeader from './components/ContactHeader';
+import ContactForm from './components/ContactForm';
+import ContactGrid from './components/ContactGrid';
 
 const App = () => {
 	return (
